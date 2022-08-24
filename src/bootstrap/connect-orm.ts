fa936ae37308925f75b4fdc1f5b4da745515ec40
Vladimir Markov <vladimir.markov@bdtech.by>
2022-08-24 03:41:16 +0300

    Updated package & update typeorm connect

--- src/bootstrap/connect-orm.ts
+++ src/bootstrap/connect-orm.ts
@@ -1,12 +1,12 @@
 import Koa from 'koa';
-import { createConnection } from 'typeorm';
+import { DataSource } from 'typeorm';
 import ormConfig from '../config/orm';
 
 export default async (app: Koa) => {
-    try {
-        app.context.orm = await createConnection(ormConfig)
-    } catch (err) {
-        console.log(err)
-        process.exit(1)
-    }
+  try {
+    app.context.orm = new DataSource(ormConfig)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
 }
